5b894e4f3e40670f662fafd67cdc0b141767d1d0
Phanty133 <vintersk@gmail.com>
2020-05-09 15:40:01 +0300

    Initial Unity commit`

---
+++ Assets/Editor/MaterialGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class MaterialGenerator
+{
+    [MenuItem("Assets/Create Materials")]
+    private static void CreateMaterials()
+    {
+
+        foreach (Object o in Selection.objects)
+        {
+
+            if (o.GetType() != typeof(Texture2D))
+            {
+                Debug.LogError("This isn't a texture: " + o);
+                continue;
+            }
+
+            Debug.Log("Creating material from: " + o);
+
+            Texture2D selected = o as Texture2D;
+
+            Material material = new Material(Shader.Find("Unlit/Texture"));
+            material.mainTexture = (Texture)o;
+
+            string savePath = AssetDatabase.GetAssetPath(selected);
+            savePath = savePath.Substring(0, savePath.LastIndexOf('/') + 1);
+
+            string newAssetName = savePath + selected.name + ".mat";
+
+            AssetDatabase.CreateAsset(material, newAssetName);
+
+            AssetDatabase.SaveAssets();
+
+        }
+        Debug.Log("Done!");
+    }
+}
---
+++ Assets/Scripts/Tracking/CameraTrackingManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraTrackingManager : MonoBehaviour
+{
+    public void SetInitialCameraPos()
+    {
+        Vector3 camPos = GameObject.FindGameObjectWithTag("MainCamera").transform.position;
+        GameObject.FindGameObjectWithTag("MainPlayerDeck").GetComponent<CardRotation>().camPos0 = camPos;
+
+        GameObject.Find("Cam0").GetComponent<Text>().text = "Cam pos0: " + camPos.ToString();
+    }
+}
